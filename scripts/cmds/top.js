7f890ed69bd634d07169cf9e61db158947fb9bb8
Shirosadx <eusiosergio@gmail.com>
2026-07-16 20:22:15 +0200

    Update top.js

--- scripts/cmds/top.js
+++ scripts/cmds/top.js
@@ -17,8 +17,8 @@ module.exports = {
     config: {
         name: "top",
         aliases: ["ranking", "maisricos"],
-        version: "1.0",
-        author: "Gerson",
+        version: "1.1",
+        author: "SeuNome",
         countDown: 5,
         role: 0,
         description: {
@@ -30,11 +30,72 @@ module.exports = {
         }
     },
 
-    onStart: async function ({ message }) {
+    onStart: async function ({ message, usersData }) {
         const data = loadData();
         const users = data.users;
         const userIds = Object.keys(users);
         
+        // 🔥 PEGA OS NOMES DO usersData E ATUALIZA NO JSON
+        const players = [];
+        
+        for (const id of userIds) {
+            if (users[id].money > 0) {
+                let name = users[id].name || `User_${id}`;
+                
+                // 🔥 TENTA PEGAR O NOME DO usersData (mais atualizado)
+                try {
+                    const userInfo = await usersData.get(parseInt(id));
+                    if (userInfo && userInfo.name) {
+                        name = userInfo.name;
+                        // Atualiza no JSON
+                        users[id].name = name;
+                    }
+                } catch (e) {}
+                
+                players.push({
+                    userId: id,
+                    money: users[id].money || 0,
+                    name: name
+                });
+            }
+        }
+
+        // Salva os nomes atualizados
+        const fs = require('fs');
+        try {
+            fs.writeFileSync(DATA_FILE, JSON.stringify(data, null, 2), 'utf8');
+        } catch (e) {}
+
+        if (players.length === 0) {
+            return message.reply("📊 | NINGUÉM TEM DINHEIRO AINDA!");
+        }
+
+        // Ordena por dinheiro
+        const sorted = players.sort((a, b) => b.money - a.money);
+        const top10 = sorted.slice(0, 10);
+
+        let msg = "🏆 **MAIS RICOS** 🏆\n\n";
+        
+        top10.forEach((user, index) => {
+            let medal = "";
+            if (index === 0) medal = "🥇 ";
+            else if (index === 1) medal = "🥈 ";
+            else if (index === 2) medal = "🥉 ";
+            else medal = `${index + 1}. `;
+            
+            msg += `${medal} **${user.name}**\n`;
+            msg += `   💰 ${user.money}$\n\n`;
+        });
+
+        // Estatísticas
+        const totalMoney = players.reduce((acc, p) => acc + p.money, 0);
+        const totalPlayers = players.length;
+        
+        msg += `📊 **TOTAL:** ${totalMoney}$ | **JOGADORES:** ${totalPlayers}`;
+
+        return message.reply(msg);
+    }
+};        
         const players = userIds
             .filter(id => users[id].money > 0)
             .map(id => ({
